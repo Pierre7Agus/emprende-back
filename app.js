fb1bad5d0042f78e82af56bf79fd365ac8893ba0
Pierre7Agus <redtor_90@outlook.es>
2023-05-07 12:01:57 -0500

    Puerto agregado

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
+require('dotenv').config();
 const http=require('http');
 let name=require("./another");
 
-console.log({name})
+//console.log({name})
+
+const PORT=process.env.PORT;
+
+
 
 function request(){
   console.log("Se ha realizado una request")
 }
 
-//http.createServer(request).listen(4000);
+
+
+http.createServer(request).listen(PORT,()=>{
+  console.log("Aplicacion corriendo en el puerto: ",PORT)
+});
